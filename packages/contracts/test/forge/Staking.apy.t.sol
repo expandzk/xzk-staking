17dfad9a5444133f722f9b01999d1bb45de207f5
ericlurker <eric@mystiko.network>
2025-07-01 11:51:21 +0800

    update, fix test error

--- packages/contracts/test/forge/Staking.apy.t.sol
+++ packages/contracts/test/forge/Staking.apy.t.sol
@@ -36,9 +36,9 @@ contract StakingApyTest is Test {
 
     function testApyNoStaking() public {
         uint256 startTime = staking.START_TIME();
-        uint256 totalReward = staking.totalRewardAt(block.timestamp + 365 days);
         vm.warp(startTime - 1 hours);
         vm.roll(10);
+        uint256 totalReward = staking.totalRewardAt(block.timestamp + 365 days);
         uint256 apyValue = staking.apy(1e18);
         assertEq(apyValue, totalReward, "APY be to total reward");
 
@@ -46,7 +46,32 @@ contract StakingApyTest is Test {
         staking.apy_staker();
     }
 
-    function testApyWithStakingTokenSupply() public {
+    function testApyAfterStakingDuration(uint256 amount) public {
+        vm.startPrank(deployer);
+        uint256 startTime = staking.START_TIME();
+        vm.warp(startTime + 2 hours);
+        vm.roll(10);
+        uint256 totalBalance = mockToken.balanceOf(deployer);
+        amount = bound(amount, 1, totalBalance);
+        mockToken.approve(address(staking), amount);
+        staking.stake(amount);
+        assertEq(staking.totalSupply(), amount);
+        assertEq(staking.totalStaked(), amount);
+        assertEq(staking.totalUnstaked(), 0);
+        vm.stopPrank();
+
+        uint256 total_duration = staking.TOTAL_DURATION_SECONDS();
+        vm.warp(startTime + total_duration + 1 seconds);
+        vm.roll(10);
+
+        uint256 apyValue = staking.apy(1e18);
+        assertEq(apyValue, 0, "APY should be zero");
+
+        uint256 apyValue2 = staking.apy(amount);
+        assertEq(apyValue2, 0, "APY should be zero");
+    }
+
+    function testApyWithStakingTokenSupply(uint256 time_passed, uint256 amount) public {
         vm.startPrank(deployer);
         uint256 startTime = staking.START_TIME();
         vm.warp(startTime - 2 hours);
@@ -59,16 +84,24 @@ contract StakingApyTest is Test {
         assertEq(staking.totalUnstaked(), 0);
         vm.stopPrank();
 
+        uint256 total_duration = staking.TOTAL_DURATION_SECONDS();
+        time_passed = bound(time_passed, startTime + 1, startTime + total_duration);
+
+        vm.warp(startTime + time_passed);
+        vm.roll(10);
+
+        uint256 totalReward = staking.totalRewardAt(block.timestamp + 365 days);
         uint256 apyValue_staker = staking.apy_staker();
-        assertEq(apyValue_staker, 500000000000000, "APY should match");
+        assertEq(apyValue_staker, (totalReward * 1e18) / totalBalance, "APY should match");
 
-        uint256 apyValue = staking.apy(1);
-        assertEq(apyValue, 0, "APY should be zero");
-
-        uint256 apyValue2 = staking.apy(1e18);
-        assertEq(apyValue2, 499999999500000, "APY should match");
+        uint256 amount1 = bound(amount, 1, totalBalance - 1);
+        uint256 amount2 = amount1 + 1;
+        uint256 apyValue1 = staking.apy(amount1);
+        uint256 apyValue2 = staking.apy(amount2);
+        assertGe(apyValue1, 0, "apy1 should be greater than 0");
+        assertGe(apyValue2, 0, "apy2 should be greater than 0");
 
         uint256 apyValue3 = staking.apy(totalBalance);
-        assertEq(apyValue3, 250000000000000, "APY should match");
+        assertLt(apyValue3, apyValue_staker, "apy3 should be less than apy_staker");
     }
 }
